0cfa9edbd16dfdc585c82b8631b8c33296f5647d
AlosyCrazy <103683617+AlosyCrazy@users.noreply.github.com>
2022-07-26 18:56:03 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+var menuListArray = ["Pizza Vegetariana", "Pizza de frango", "Pizza Portuguêsa", "Pizza Quatro Queijos", "Pizza de calabresa", "Pizza Marguerita"];
+function getMenu(){
+var htmldata = "";
+menuListArray.sort();
+for (var contador = 0; contador<menuListArray.length; contador++){
+    htmldata = htmldata + menuListArray[contador]+"<br>";
+}
+document.getElementById("displayMenu").innerHTML = htmldata;
+}
+
+function addItem(){
+var htmldata = "";
+var item=document.getElementById("addItem").value;
+var imgTags = '<img src="images/pizzaImg.png" id="im1">';
+menuListArray.sort();
+for (var contador = 0; contador<menuListArray.length; contador++){
+    htmldata = htmldata + imgTags + menuListArray[contador]+"<br>";
+}
+document.getElementById("displayAddedMenu").innerHTML = htmldata;
+
+}
+
+function addTop(){
+var item = document.getElementById("addItem").value;
+menuListArray.push(item);
+addItem();
+}
